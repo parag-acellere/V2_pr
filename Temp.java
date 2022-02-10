6cba2ccac88be7b9c5f06683e3699deb27d513b2
parag-acellere <parag.dalal@embold.io>
2022-02-10 17:10:31 +0530

    MBLD2020-5276

--- Temp.java
+++ Temp.java
@@ -2,11 +2,9 @@ public class Temp{
 public static void main(String args[]){
 int categoryid;
 
+System.out.println("Temp msg 1")
     final int finalfield = 1;
 System.out.println("Temp msg 2")
 
-final int finalfield2 = 1;
-
-System.out.println("Temp msg 1")
 }
 }
