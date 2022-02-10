36d33f446204f1687d9d9906897d5000b49d5ab0
parag-acellere <parag.dalal@embold.io>
2022-02-10 17:06:23 +0530

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -1,7 +1,9 @@
 public class Temp{
 public static void main(String args[]){
 int categoryid;
+
+System.out.println("Temp msg 1")
     final int finalfield = 1;
-System.out.println("Temp msg")
+System.out.println("Temp msg 2")
 }
 }
